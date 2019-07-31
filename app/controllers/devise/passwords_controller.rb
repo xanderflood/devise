d95bc2f104dde6b93a3a664faedb78e5d8d9bed2
Xander Flood <xander.t.flood@gmail.com>
2019-07-31 14:52:03 -0400

    add debug logging

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -63,6 +63,9 @@ class Devise::PasswordsController < DeviseController
 
     # Check if a reset_password_token is provided in the request
     def assert_reset_token_passed
+      puts "dsfiasdf98sdf80"
+      puts params
+      puts "dsfiasdf98sdf80"
       if params[:reset_password_token].blank?
         set_flash_message(:alert, :no_token)
         redirect_to new_session_path(resource_name)
